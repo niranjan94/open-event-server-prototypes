47ed821d3f98a02567ea8a1b4621d611d646a628
Niranjan Rajendran <niranjan94@yahoo.com>
2016-05-21 12:03:32 +0530

    Drag and drop primitive

--- scheduling/js/scheduler.js
+++ scheduling/js/scheduler.js
@@ -3,42 +3,52 @@ interact('.event')
         // enable inertial throwing
         inertia: true,
         // keep the element within the area of it's parent
-        restrict: {
-            restriction: "parent",
-            endOnly: true,
-            elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-        },
         // enable autoScroll
         autoScroll: true,
-
         // call this function on every dragmove event
-        onmove: dragMoveListener,
+        onmove: function (event) {
+            var target = event.target,
+                x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+                y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+            target.style.webkitTransform =
+                target.style.transform =
+                    'translate(' + x + 'px, ' + y + 'px)';
+
+            target.setAttribute('data-x', x);
+            target.setAttribute('data-y', y);
+        },
         // call this function on every dragend event
         onend: function (event) {
-            var textEl = event.target.querySelector('p');
 
-            textEl && (textEl.textContent =
-                'moved a distance of '
-                + (Math.sqrt(event.dx * event.dx +
-                    event.dy * event.dy)|0) + 'px');
         }
     });
 
-function dragMoveListener (event) {
-    var target = event.target,
-    // keep the dragged position in the data-x/data-y attributes
-        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-    // translate the element
-    target.style.webkitTransform =
-        target.style.transform =
-            'translate(' + x + 'px, ' + y + 'px)';
+interact('.room-inner').dropzone({
+    // only accept elements matching this CSS selector
+    accept: '.event',
+    // Require a 75% element overlap for a drop to be possible
+    overlap: 0.50,
 
-    // update the posiion attributes
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-}
+    ondropactivate: function (event) {
+        event.target.classList.add('drop-active');
+    },
+    ondragenter: function (event) {
+        event.target.classList.add('drop-now');
+    },
+    ondragleave: function (event) {
+        event.target.classList.remove('drop-now');
+    },
+    ondrop: function (event) {
+        event.relatedTarget.classList.remove('unscheduled');
+        event.relatedTarget.classList.add('scheduled');
+        event.target.classList.remove('drop-active');
+        event.target.classList.remove('drop-now');
 
-// this is used later in the resizing and gesture demos
-window.dragMoveListener = dragMoveListener;
+    },
+    ondropdeactivate: function (event) {
+        event.target.classList.remove('drop-now');
+        event.target.classList.remove('drop-active');
+    }
+});
